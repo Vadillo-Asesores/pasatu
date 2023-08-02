a11bc5d92f5f16c3df0957f7c94067b9eca8bf51
Mikel Gutiérrez <mgutierrez@grupovadillo.com>
2023-08-02 12:18:41 +0200

    Métodos para extracción IP de los domain controller

---
+++ ADUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pasatu
+{
+    class ADUtils
+    {
+        public static DomainControllerCollection GetDCServers() => Domain.GetCurrentDomain().FindAllDiscoverableDomainControllers();
+
+        /// <summary>
+        /// Returns a list with all the AD Controllers IP with a non null IP.
+        /// </summary>
+        /// <returns></returns>
+        public static List<IPAddress> GetDCServersIP()
+        {
+            DomainControllerCollection adCol = ADUtils.GetDCServers();
+            List<IPAddress> ipList = new List<IPAddress>();
+
+            foreach (DomainController controller in adCol)
+            {
+                if (controller.IPAddress != null) { 
+                    ipList.Add(IPAddress.Parse(controller.IPAddress));
+                }
+            }
+
+            return ipList;
+
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Pasatu
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            ADUtils.GetDCServersIP();
+        }
     }
 }
